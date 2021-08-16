8717cea1fb7f1e0518e75216a29d471c6066eaca
Aurélien <aurelien.monceau@gmail.com>
2021-08-17 01:11:15 +0200

    Retouche du design de la page d'accueil + affichage de l'id dans console log => page produit

---
+++ front-end/js/produit.js
@@ -0,0 +1,34 @@
+
+
+
+
+ (async function() {
+    const articleId = getArticleId()
+    console.log(articleId)
+    const article =  getArticle(articleId)
+   /*  displayArticle(articleId) */
+    
+    /* console.log(o); */
+  }) ()
+  
+  function getArticleId() {
+    return new URL(location.href).searchParams.get('id')
+  }
+  
+  function getArticle(articleId) {
+     return fetch("http://localhost:3000/api/teddies/" + articleId)
+      .then(function(httpBodyResponse) {
+        return httpBodyResponse.json()
+      }) 
+      .then(function(article) {
+        return article
+      }) 
+    /*   .catch(function(error) {
+        alert(error)
+      })  */
+  }
+ /*  
+ function displayArticle(article) {
+   document.getElementById("ourson").textContent = article.name
+    document.getElementById("ourson1").textContent = article.price 
+  }   */

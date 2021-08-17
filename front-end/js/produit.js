15a11431d99e0e5b5a8201a06205406aa8feff66
Aurélien <aurelien.monceau@gmail.com>
2021-08-17 19:37:17 +0200

    Correction des erreurs scripts JS / Page produit

--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -1,34 +1,33 @@
 
 
-
-
  (async function() {
-    const articleId = getArticleId()
-    console.log(articleId)
-    const article =  getArticle(articleId)
-   /*  displayArticle(articleId) */
-    
-    /* console.log(o); */
-  }) ()
+  const articleId = getArticleId()
+  console.log(articleId)
+  const article = await getArticle(articleId)
+  /* console.log(article) */
+  displayArticle(article) 
   
-  function getArticleId() {
-    return new URL(location.href).searchParams.get('id')
-  }
-  
-  function getArticle(articleId) {
-     return fetch("http://localhost:3000/api/teddies/" + articleId)
-      .then(function(httpBodyResponse) {
-        return httpBodyResponse.json()
-      }) 
-      .then(function(article) {
-        return article
-      }) 
-    /*   .catch(function(error) {
-        alert(error)
-      })  */
-  }
- /*  
- function displayArticle(article) {
-   document.getElementById("ourson").textContent = article.name
-    document.getElementById("ourson1").textContent = article.price 
-  }   */
+  /* console.log(o); */
+}) ()
+
+function getArticleId() {
+  return new URL(location.href).searchParams.get('id')
+}
+
+function getArticle(articleId) {
+   return fetch("http://localhost:3000/api/teddies/" + articleId)
+    .then(function(httpBodyResponse) {
+      return httpBodyResponse.json()
+    }) 
+    .then(function(article) {
+      return article
+    }) 
+  /*   .catch(function(error) {
+      alert(error)
+    })  */
+}
+
+function displayArticle(article) {
+ document.getElementById("ourson").textContent = article.name
+  document.getElementById("ourson1").textContent = article.price 
+}  

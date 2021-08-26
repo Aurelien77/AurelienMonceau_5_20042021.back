1361e679a807ba92cceac223cc11542af7fa402b
Aurélien <aurelien.monceau@gmail.com>
2021-08-26 23:11:10 +0200

    commentaire apporté sur les fichiers index.js + produit.js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,67 +1,63 @@
 
-//La console indique en logs : 5 sections de nounours 
-/* class Teddies {
-constructor(jsonTeddies){
+//La console indique en logs : 5 sections Teddies
+//Les descritifs des sections de l'API apparaissent dans Postman et dans le console log de la réponse JSON.
+// colors, _id, name, price, imageUrl.
 
-  jsonTeddies  && Object.assign(this, jsonTeddies);
-}
-
-
-} */
-
-
-
-
-
-/* fetch('http://localhost:3000/api/teddies')
-.then( data => data.json())
-
-
-.then ( jsonListTeddies=> {
-/* console.log(jsonListArticle); */
-/* for {let jsonTeddies  of jsonListTeddies) {
-let Teddie = new Teddies (jsonTeddies);
-document.getElementById('ourson1').innerHTML += data.name ;
-
-
-}
-
-}); */
-
-//création d'une requête vers l'api qui est renvoyé au fichier .json
+try {
+//création d'une requête vers l'api qui est une réponse au format .json
 const product_sheet = fetch("http://localhost:3000/api/teddies")
     .then((response) => {
         return response.json();
     })
+    // Itération sur la réponse 
     .then((response) => {
-        for (let o of response) {      //création d'une variable appelée "O" avec les résultats de la requête.json
+        for (let o of response) {      //création d'une variable appelée "O" avec les résultats de l'itération de la requête.json
             console.log(o);
              //On pointe l'id "OursonTemplate"
            
             const Template = document.getElementById("OursonTemplate");
 
-            //On cré uone constante avec l'importation de tous les noeuds "<template> .. .. </template>" 
+            //On crée une constante avec l'importation de tous les éléments HTML qui se trouvent dans "<template> .. .. </template>" (noeuds)
             const CopieTemplate = document.importNode(Template.content, true);
- //les noeuds du templates sont importés pour accueillir la constante "o" du fichier.json + les sections de l'API
- /* sections de l'API reccueillit lors du  "console.log(o)" */
 
-  /* <!> pas de innerHTML += Securité */
-  /* Varialbe o + informations récupérées lors du concole.log */
- CopieTemplate.getElementById("imagesours").src = o.imageUrl;
+
+
+  // On copie le template HTML dans l'élément pointé avec la commande getelementByid puis on injecte du html avec .textContent suivit de la valeur stocké dans la réponse JSON (variable "o")
+
+  
+// Pas de innerHTML pour plus de Securité.
+ 
+ CopieTemplate.getElementById("imagesours").src = o.imageUrl;    //.src pour le lien URL de l'image + variable de la réponse JSON.valeur
             CopieTemplate.getElementById("nomsours").textContent = o.name;
             CopieTemplate.getElementById("prixours").textContent= o.price / 100 + "€";
-            /* Création de l'appendchild (élément enfant) sous le calque avec l'ID "sections" => ici la copie du template x 5*/
                 CopieTemplate.getElementById("descriptionours").textContent= o.description;
-  /* absence de guillemet ou de cotes pour o + valeurs / afin d'ajouter du texte et l'opération qui divise les centimes*/
 
-/* Définition de l'envoi de l'ID vers l'url */
+
+//On récupère l'ID sous la forme d'un lien : .href + on ajoute un GET ID qui enverra l'ID dans L'URL une fois le lien cliqué.
  CopieTemplate.getElementById("link").href += "?id=" + o._id;
 
+//On copie celon le nombre d'itération le Template dans la div "sections"
 
             document.getElementById("sections").appendChild(CopieTemplate);
         }   
-    });  
+   
+   
+      }
+      
+      
+      
+      
+      
+      );  
   
+
+    } catch (error) {
+      console.error(error);
+      // expected output: ReferenceError: nonExistentFunction is not defined
+      // Note - error messages will vary depending on browser
+    }
+
+    /* 
     localStorage.setItem (
       "new4", "nouvelle3",
      
@@ -73,7 +69,7 @@ const product_sheet = fetch("http://localhost:3000/api/teddies")
         console.log(event)
         
         })
- 
+  */
 
 
 
@@ -111,24 +107,6 @@ const array = [1,2,3];
 document.querySelector('affichourson').innerHTML += o.name +=o.imageUrl +=  o.colors +=  o.price  += o.description+= o._id += "-------<p>Son nom, sa couleur, son prix, l'url de son image sa déscription et son Id</p>-------";
 document.querySelector('oursonImg').innerHTML += o.imageUrl; */
 /* ------------------------------------------------------------------------------------------ */
-
-
-
-
-
-      
-
-  
-
-
-
-
-
-
-
-
-
-
 
 
 
--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -1,109 +1,135 @@
 
 
 
-
+// ON STOCK L'ID récupéré dans l'URL dans une fonction Asynchrone
 //----------------------------------------------------
-
- /* création de la fonction articleid qui aura pour paramêtre 
- L'id retrouné par la fonction getArticleId */
- 
- (async function() {
-  const articleId = getArticleId()
-/*   console.log(articleId) */
-  const article = await getArticle(articleId)
-  /* console.log(article) */
-  displayArticle(article) 
-  
-  /* console.log(o); */
-}) ()
-
-
-
-function getArticle(articleId) {
-  return fetch("http://localhost:3000/api/teddies/" + articleId)
-   .then(function(o) {
-     return o.json()
-   }) 
-   .then(function(article) {
-     return article
-   }) 
- /*   .catch(function(error) {
-     alert(error)
-   })  */
-}
-
-
- /* On stock dans une fonction l'id enregistré dans les paramêtres URL */
-function getArticleId() {
+ // Objet n° 1- Objet getArticleId
+ //----------------------------------
+// stocke L'ID envoyé par L'URL --- searchParams.get('id') permet de rechercher la valeur ID dans les paramètres location de la page.
+ /* console.log(location); */ // log les paramêtres location de la page web
+ function getArticleId() {
   return new URL(location.href).searchParams.get('id')
 }
 
 
-
-function displayArticle(article) {
- document.getElementById("nomsours").textContent = article.name
-  document.getElementById("prixours").textContent  = article.price / 100 + "€" 
-  document.getElementById("imagesours").src = article.imageUrl;
- 
-  for (const couleurs of article.colors) { 
-  document.getElementById("option_produit").innerHTML +=  "<option value ='"+couleurs+"'>" + couleurs + "</option>";
-}  
-
+ // Objet n° 2- Objet  getArticle ( ID )    
+//----------------------------------
+// Renvoi les donnée de l'API  choisit par ID  
+//NB : Param articleId est une constante dans une fonction ( asynchrone + await  )   
+//articleId (renvoit les données API)  = ArticleId (renvoit l'ID )      ArticleId = getArticleId
+function getArticle(articleId) {
+  return fetch("http://localhost:3000/api/teddies/" + articleId)   //http://localhost:3000/api/teddies/   numéro de L'ID
+   .then(function(response) {
+     return response.json()
+     
+   } 
+   ) 
+  
 }
+console.log();
+//--------------------------------------------------
+ // Objet n° 3- Objet displayArticle + article ( id )
+ //----------------------------------
+// On pointe les élément HTML pour injecter le contenu de l'API lié à L'id. 
+function displayArticle(article) {
+  document.getElementById("nomsours").textContent = article.name   // article.name = réponse JSON+ID .name
+   document.getElementById("prixours").textContent  = article.price / 100 + "€" 
+   document.getElementById("imagesours").src = article.imageUrl;
+   // Itération celon le nombre de couleurs présentes dans la réponse JSON + injection dans des balises HTML
+   for (const couleurs of article.colors) { 
+   document.getElementById("option_produit").innerHTML +=  "<option value ='"+couleurs+"'>" + couleurs + "</option>";
+ }  
+ 
+ }
+
+ //--------------------------------------------------
+ // Objet n° 4- La fonction asynchrone
+//  Distribut les paramêtres de l'ID aux fonctions qui appellent articleId +
+ //  Distribut la réponse JSON aux fonctions qui appellent article  
+ // Déploit le code HTML par l'appelle de la fonction displayArticle(article) 
+ (async function() {
+  const articleId = getArticleId() 
+
+  const article = await getArticle(articleId)
+  
+
+  displayArticle(article) 
+  
+
+}) ()
+
+
+
+
+
+
 
 function roundToTwo(num) {    
     return +(Math.round(num + "e+2")  + "e-2");
 }
 
-
-//Ecouter le bouton et envoyer au panier
+ //--------------------------------------------------
+ //  5- addEventListener : Bouton envoyer vers le panier
+//Ecouter le bouton envoyer vers le panier
 document.getElementById("btn-envoyer").addEventListener("click", (event) => {
 
-  event.preventDefault();
-  const articleId = getArticleId();
-  const couleurChoisi = document.getElementById("option_produit").value;
-  var qte = parseInt(document.getElementById("qte_produit").value);
+  event.preventDefault();  //Pour ne pas changer de page au clic du bouton
 
+
+
+  // définition des Constantes pour Objet n°6 
+  const articleId = getArticleId();    
+  //Pointer les élements HTML pour récupérer les valeurs qui s'y trouvent
+  const couleurChoisi = document.getElementById("option_produit").value;   // Pointe le div  Select . value = <OptionS>
+  const qte = parseInt(document.getElementById("qte_produit").value);   //Pointe l'élement html avec parseInt : Si l'argument string n'est pas une chaîne de caractères, elle sera convertie en une chaîne (Pour Envoi au local storage) 
+ const nomprd = document.getElementById("nomsours").textContent;    // On notes .textcontent car on recupère du texte et non plus les valeurs ( sous catégories ) d'un éléments.
+ const prixXquantite = roundToTwo((parseInt(document.getElementById("prixours").textContent))*qte);
+
+  //--------------------------------------------------
+   //  Objet n° 6- Récupérer les valeur du formulaire dans un objet
   
-  // recuperation des valeurs du formulaire
   let optionProduit = {
-      nomProduit: document.getElementById("nomsours").textContent,
+      nomProduit: nomprd,
       idProduitSelectionner: articleId,
       option_produit : couleurChoisi,
       quantite: qte,
-      prix: roundToTwo((parseInt(document.getElementById("prixours").textContent))*qte)
+      prix: prixXquantite
 	  }
       
-    console.log(optionProduit);
+    console.log(optionProduit); 
 	
+  //--------------------------------------------------
+   // n° 7- Récupérer les valeur dans le local storage
+
+
 	// definition de la nouvelle liste des produits a enregistrer
 	let nouvelleListeProduit = [];
 
 //test si le local storage n'est pas vide
 	if (localStorage !== null) {
 		//recuperations de la liste des produits deja enregistre dans le local storage
-		let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
+		let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit")); //.parse permet de convertir les données JSON en JS, produit et le nom donnée à la clée dans le local storage
 		// copie de la liste des produit deja enregistre dans le local storage
 		nouvelleListeProduit = produitEnregistreDansLocalStorage;
 		
 		//parcourir la liste des produit deja enregistre
-		for (let indexTab in produitEnregistreDansLocalStorage) { 
+		/* for (let indexTab in produitEnregistreDansLocalStorage) {   // indextab ou I (...) est identique 
 			//teste si le produit enregistré dans le local storage a le meme id que le produit que je veux enregistrer
 			if(produitEnregistreDansLocalStorage[indexTab].idProduitSelectionner.localeCompare(optionProduit.idProduitSelectionner) == 0){
 				//dans la copie de la liste des produits je retire ce produit que je veux enregister 
 				nouvelleListeProduit.splice(indexTab, 1);
 			}
-		}
+		} */
 	}
 	
 	// tester si la nouvelle liste n'est pas nulle
 	if (nouvelleListeProduit !== null) {
 		nouvelleListeProduit.push(optionProduit);
-	}else{ //sinon c'est a dire elle est nulle ou vide
+	}else{ //sinon elle est vide
 		nouvelleListeProduit = [];
 		nouvelleListeProduit.push(optionProduit);
 	}
 	//finalement j'enregistre la nouvelle liste dans le localstorage
-	localStorage.setItem("produit",JSON.stringify(nouvelleListeProduit));
+	localStorage.setItem("produit",JSON.stringify(nouvelleListeProduit)); //.stringify permet de convertir les données JS en JSON 
   
   });
